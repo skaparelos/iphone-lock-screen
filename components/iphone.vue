30944cdd79083997f2e05b055c174358a5bdd1e7
skaparelos <skaparelos@gmail.com>
2022-02-03 22:50:01 +0200

    tailwind in JIT

---
+++ components/iphone.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="w-[75px] h-[100px] bg-black">
+        hello world
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <!-- <Tutorial/> -->
+  <iphone />
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
